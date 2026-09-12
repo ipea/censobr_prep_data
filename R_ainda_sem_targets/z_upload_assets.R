363ee65ec64872165ca91a8ec15f440c3ec119e1
Rogerio Barbosa <antrologos@gmail.com>
2026-09-12 10:24:08 -0300

    fix(upload): aponta o pb_upload para a nova organizacao
    Os dois repositorios sairam de ipeaGIT para ipea. Operacao de git segue o
    redirecionamento do GitHub, mas o piggyback sobe asset pela API REST, e
    requisicao de upload costuma nao seguir redirect -- seria uma falha bem na
    hora de publicar os parquets.

--- R_ainda_sem_targets/z_upload_assets.R
+++ R_ainda_sem_targets/z_upload_assets.R
@@ -7,5 +7,5 @@ parquet_files <- list.files('./data/',
                             recursive = T, pattern = '_v0.3.0.parquet')
 
 piggyback::pb_upload(file = parquet_files,
-                     repo = 'ipeaGIT/censobr',
+                     repo = 'ipea/censobr',
                      tag = 'v0.3.0')
